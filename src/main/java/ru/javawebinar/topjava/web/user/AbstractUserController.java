288180ae56551a4f4e2b5a7531bbee293382122e
Pavel Galushkin <vinsent666@gmail.com>
2021-04-14 15:41:05 +0300

    binding

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.UserUtil;
 
 import java.util.List;
 
@@ -27,6 +29,12 @@ public abstract class AbstractUserController {
         return service.get(id);
     }
 
+    public void create(UserTo userTo) {
+        log.info("create from to {}", userTo);
+        checkNew(userTo);
+        create(UserUtil.createNewFromTo(userTo));
+    }
+
     public User create(User user) {
         log.info("create {}", user);
         checkNew(user);
@@ -42,6 +50,12 @@ public abstract class AbstractUserController {
         log.info("update {} with id={}", user, id);
         assureIdConsistent(user, id);
         service.update(user);
+    }
+
+    public void update(UserTo userTo, int id) {
+        log.info("update {} with id={}", userTo, id);
+        assureIdConsistent(userTo, id);
+        service.update(userTo);
     }
 
     public User getByMail(String email) {
